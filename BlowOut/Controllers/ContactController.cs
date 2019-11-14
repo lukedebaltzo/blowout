a60d28a2b39705b4eb828e7ea06074e4995aff66
Luke DeBaltzo <lukedebaltzo@gmail.com>
2019-11-13 19:29:50 -0700

    Add project files.

---
+++ BlowOut/Controllers/ContactController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlowOut.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            ViewBag.phone = "Please call Support at <strong><u>801-555-1212.</u></strong> Thank you!";
+            return View();
+        }
+
+        public ActionResult Email(string sName, string sEmail)
+        {
+            ViewBag.email = "Thank you " + sName + ". We will send an email to " + sEmail;
+            return View();
+        }
+    }
+}
