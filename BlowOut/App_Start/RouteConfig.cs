3f67c7c3b223f3807c0a931792e51c28dcd264d1
Luke DeBaltzo <lukedebaltzo@gmail.com>
2019-11-15 22:20:02 -0700

    change the route config to allow for different urls to access

--- BlowOut/App_Start/RouteConfig.cs
+++ BlowOut/App_Start/RouteConfig.cs
@@ -12,12 +12,16 @@ namespace BlowOut
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.MapRoute(
+                "Email",
+                "Contact/Email/{sName}/{sEmail}",
+                new { Controller = "Contact", Action = "Email" });
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            
         }
     }
 }
