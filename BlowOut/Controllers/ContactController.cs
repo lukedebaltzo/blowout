6443baca0d67c777606f9910d5a83d7cce232e04
Luke DeBaltzo <lukedebaltzo@gmail.com>
2019-11-15 23:36:20 -0700

    here is some of the partial code for the sending emails

--- BlowOut/Controllers/ContactController.cs
+++ BlowOut/Controllers/ContactController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Mail;
+
 
 namespace BlowOut.Controllers
 {
@@ -18,6 +20,20 @@ namespace BlowOut.Controllers
         public ActionResult Email(string sName, string sEmail)
         {
             ViewBag.email = "Thank you " + sName + ". We will send an email to " + sEmail;
+
+            /**This was the code we worked on to send the email but it kept bugging out on the client.send part
+             * MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress("ishomework2.3@gmail.com");
+            mailMessage.To.Add(new MailAddress("puker514@aol.com"));
+            mailMessage.Subject = "Your subject";
+            mailMessage.IsBodyHtml = true;
+            mailMessage.Body = "Email body";
+            SmtpClient client = new SmtpClient();
+            client.Credentials = new System.Net.NetworkCredential("ishomework2.3@gmail.com", "abcd1234!@#$");
+            client.Host = "smtp.gmail.com";
+            client.Send(mailMessage);**/
+
+
             return View();
         }
     }
